871cc1266629309b714cc020d568fa01bb67b21a
xrc2 <109512837+9xbt@users.noreply.github.com>
2025-04-07 17:57:25 +0200

    bentobox: elf symbols

--- include/kernel/elf64.h
+++ include/kernel/elf64.h
@@ -32,14 +32,31 @@ typedef int64_t  Elf64_Sxword;
 #define PT_SHLIB    5
 #define PT_PHDR     6
 
+#define SHT_NULL        0
+#define SHT_PROGBITS    1
+#define SHT_SYMTAB      2
+#define SHT_STRTAB      3
+#define SHT_RELA        4
+#define SHT_HASH        5
+#define SHT_DYNAMIC     6
+#define SHT_NOTE        7
+#define SHT_NOBITS      8
+#define SHT_REL         9
+#define SHT_SHLIB       10
+#define SHT_DYNSYM      11
+#define SHT_LOOS        0x60000000
+#define SHT_HIOS        0x6FFFFFFF
+#define SHT_LOPROC      0x70000000
+#define SHT_HIPROC      0x7FFFFFFF
+
 typedef struct {
     unsigned char e_ident[EI_NIDENT];
     Elf64_Half e_type;
     Elf64_Half e_machine;
     Elf64_Word e_version;
     Elf64_Addr e_entry;
-    Elf64_Off e_phoff; 
-    Elf64_Off e_shoff; 
+    Elf64_Off  e_phoff; 
+    Elf64_Off  e_shoff; 
     Elf64_Word e_flags; 
     Elf64_Half e_ehsize;
     Elf64_Half e_phentsize;
@@ -48,6 +65,28 @@ typedef struct {
     Elf64_Half e_shnum;
     Elf64_Half e_shstrndx;
 } Elf64_Ehdr;
+
+typedef struct {
+	Elf64_Word  sh_name;
+	Elf64_Word  sh_type;
+	Elf64_Xword sh_flags;
+	Elf64_Addr  sh_addr;
+	Elf64_Off   sh_offset;
+	Elf64_Xword sh_size;
+	Elf64_Word  sh_link;
+	Elf64_Word  sh_info;
+	Elf64_Xword sh_addralign;
+	Elf64_Xword sh_entsize;
+} Elf64_Shdr;
+
+typedef struct {
+	Elf64_Word    st_name;
+	unsigned char st_info;
+	unsigned char st_other;
+	Elf64_Half    st_shndx;
+	Elf64_Addr    st_value;
+	Elf64_Xword   st_size;
+} Elf64_Sym;
 
 typedef struct {
 	Elf64_Word  p_type;
@@ -60,4 +99,10 @@ typedef struct {
 	Elf64_Xword p_align;
 } Elf64_Phdr;
 
-int elf_module(struct multiboot_tag_module *mod);
+typedef struct {
+	Elf64_Sym *symtab;
+	char *strtab;
+	int  symbol_count;
+} Elf64;
+
+Elf64 *elf_module(struct multiboot_tag_module *mod);
--- kernel/misc/elf64.c
+++ kernel/misc/elf64.c
@@ -1,35 +1,66 @@
 #include <errno.h>
+#include <kernel/mmu.h>
 #include <kernel/elf64.h>
 #include <kernel/printf.h>
 #include <kernel/string.h>
 #include <kernel/multiboot.h>
 
-int elf_module(struct multiboot_tag_module *mod) {
+char *elf_get_symbol_name(Elf64 *elf, Elf64_Addr addr) {
+    for (int i = 0; i < elf->symbol_count; i++) {
+        if (elf->symtab[i].st_value == addr) {
+            return &elf->strtab[elf->symtab[i].st_name];
+        }
+    }
+    return "(none)";
+}
+
+Elf64 *elf_module(struct multiboot_tag_module *mod) {
     printf("elf: loading module \"%s\"\n", mod->string);
 
     Elf64_Ehdr *ehdr = (Elf64_Ehdr *)(uintptr_t)mod->mod_start;
 
     if (memcmp(ehdr->e_ident, "\x7f""ELF", 4)) {
         printf("elf: invalid elf file\n");
-        return -EINVAL;
+        return NULL;
     }
 
     if (ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
         printf("elf: unsupported elf class\n");
-        return -EINVAL;
+        return NULL;
+    }
+
+    Elf64_Shdr *shdr = (Elf64_Shdr *)(mod->mod_start + ehdr->e_shoff);
+    Elf64_Sym *symtab = NULL;
+    char *strtab = NULL;
+    int i, symbol_count = 0;
+
+    for (i = 0; i < ehdr->e_shnum; i++) {
+        if (shdr[i].sh_type == SHT_STRTAB && ehdr->e_shstrndx != i) {
+            strtab = (char *)(mod->mod_start + shdr[i].sh_offset);
+        } else if (shdr[i].sh_type == SHT_SYMTAB) {
+            symtab = (Elf64_Sym *)(mod->mod_start + shdr[i].sh_offset);
+            symbol_count = shdr[i].sh_size / shdr[i].sh_entsize;
+        }
+    }
+
+    for (i = 0; i < symbol_count; i++) {
+        printf("elf: addr=0x%lx, symbol=%s\n", symtab[i].st_value, &strtab[symtab[i].st_name]);
     }
 
     Elf64_Phdr *phdr = (Elf64_Phdr *)(mod->mod_start + ehdr->e_phoff);
 
-    for (int i = 0; i < ehdr->e_phnum; i++) {
-        switch (phdr[i].p_type) {
-            case PT_LOAD:
-                // TODO: load segment
-                continue;
-            default:
-                continue;
+    for (i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type == PT_LOAD) {
+            // TODO: load segment
         }
     }
 
-    return 0;
+    Elf64 *elf = kmalloc(sizeof(Elf64));
+    elf->symtab = symtab;
+    elf->strtab = strtab;
+    elf->symbol_count = symbol_count;
+
+    printf("elf: %s\n", elf_get_symbol_name(elf, 0x401000));
+
+    return NULL;
 }
